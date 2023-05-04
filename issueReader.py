23bad197241ac4a0b9b19be0bd3e777236ee0532
smizo424 <smizo424@student.otago.ac.nz>
2023-05-04 18:44:59 +1200

    Prints out the issue to the comment

--- issueReader.py
+++ issueReader.py
@@ -18,6 +18,11 @@ falseForCausedBy = []
 trueForIssue = []
 falseForIssue = []
 
+# Arrays for sentences and their corresponding issue numbers
+itWasSentences = []
+causedBySentences = []
+issueSentences = []
+
 # A function to check if a sentence contains a specific phrase, with spell-checking
 
 
@@ -32,36 +37,51 @@ def contains_phrase(sentence, phrase):
 
 
 # Check each sentence for the phrases "it was" and "caused by"
-for cell in sheet['C']:
-    sentence = str(cell.value)
+for row in sheet.iter_rows(min_row=2, min_col=1, max_col=3):
+    issue_num = str(row[0].value)
+    sentence = str(row[2].value)
+
     if contains_phrase(sentence, "it was"):
         trueForItWas.append(sentence)
+        itWasSentences.append((issue_num, sentence))
     else:
         falseForItWas.append(sentence)
 
-for cell in sheet['C']:
-    sentence = str(cell.value)
     if contains_phrase(sentence, "cause"):
         trueForCausedBy.append(sentence)
+        causedBySentences.append((issue_num, sentence))
     else:
         falseForCausedBy.append(sentence)
 
-
-for cell in sheet['C']:
-    sentence = str(cell.value)
     if contains_phrase(sentence, "issue"):
         trueForIssue.append(sentence)
+        issueSentences.append((issue_num, sentence))
     else:
         falseForIssue.append(sentence)
-# Print the results
-print("\nIT_WAS ARRAYS:")
-print("\nTRUE IT WAS statements:\n", trueForItWas)
-print("\nFALSE IT WAS statements:\n", falseForItWas)
 
-print("\nCAUSED_BY ARRAYS:")
-print("\nTRUE CAUSED BY statements:\n", trueForCausedBy)
-print("\nFALSE CAUSED BY statements:\n", falseForCausedBy)
 
-print("\nISSUE ARRAYS:")
-print("\nTRUE ISSUE statements:\n", trueForIssue)
-print("\nFALSE ISSUE statements:\n", falseForIssue)
+# # Print the results
+# print("\nIT_WAS ARRAYS:")
+# print("\nTRUE IT WAS statements:\n", trueForItWas)
+# print("\nFALSE IT WAS statements:\n", falseForItWas)
+
+# print("\nCAUSED_BY ARRAYS:")
+# print("\nTRUE CAUSED BY statements:\n", trueForCausedBy)
+# print("\nFALSE CAUSED BY statements:\n", falseForCausedBy)
+
+# print("\nISSUE ARRAYS:")
+# print("\nTRUE ISSUE statements:\n", trueForIssue)
+# print("\nFALSE ISSUE statements:\n", falseForIssue)
+
+# Print the sentences and their corresponding issue numbers
+print("\nIT_WAS SENTENCES:")
+for issue_num, sentence in itWasSentences:
+    print(f"Issue {issue_num}: {sentence}")
+
+print("\nCAUSED_BY SENTENCES:")
+for issue_num, sentence in causedBySentences:
+    print(f"Issue {issue_num}: {sentence}")
+
+print("\nISSUE SENTENCES:")
+for issue_num, sentence in issueSentences:
+    print(f"Issue {issue_num}: {sentence}")
