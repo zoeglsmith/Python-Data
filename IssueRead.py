a8e554d48bfee5e50929e46d8f7f129528481104
smizo424 <smizo424@student.otago.ac.nz>
2023-05-13 19:14:44 +1200

    issues with lammenisation and stemming imports

--- IssueRead.py
+++ IssueRead.py
@@ -1,5 +1,20 @@
+
+import nltk
+from nltk.corpus import wordnet
+from nltk.stem import PorterStemmer, WordNetLemmatizer
 import psycopg2
 import openpyxl
+import re
+
+# Set the data path for NLTK to include the absolute path to the "wordnet" folder
+nltk.data.path.append('wordnet')
+
+# Download the WordNet corpus if not already downloaded
+nltk.download('wordnet')
+
+
+# Rest of the code...
+
 
 # Constants for database connection
 DATABASE_NAME = "issues"
@@ -7,15 +22,18 @@ DATABASE_USER = "zoe"
 DATABASE_PASSWORD = "password"
 
 # Constants for specific phrases
-PHRASES = ["bugs", "problems", "issues", "causes", "it_was", "errors"]
+PHRASES = ["bugs", "problem", "issues", "cause", "it was", "errors"]
 
 # Function to check if a sentence contains a phrase
 
 
 def contains_phrase(sentence, phrases):
     for phrase in phrases:
-        if phrase.lower() in sentence.lower():
+        pattern = r"\b" + re.escape(phrase) + r"\b"
+        if re.search(pattern, sentence, re.IGNORECASE):
+            print("Found phrase:", phrase)
             return True
+    print("No matching phrase found.")
     return False
 
 # Function to connect to the database using a context manager
@@ -31,6 +49,10 @@ def connect_to_database(database_name, database_user, database_password):
     return conn
 
 
+# Initialize stemmer and lemmatizer
+stemmer = PorterStemmer()
+lemmatizer = WordNetLemmatizer()
+
 # Load the Excel file into a workbook object
 workbook = openpyxl.load_workbook('dataset.xlsx')
 
@@ -42,24 +64,39 @@ sentences = []
 issueCodes = []
 
 # Check each sentence for the phrases
-for row in sheet.iter_rows(min_row=2, min_col=1, max_col=3):
-    issue_num = str(row[0].value)
-    sentence = str(row[2].value)
-    if contains_phrase(sentence, PHRASES):
-        sentences.append((issue_num, sentence))
-        issueCodes.append((issue_num, issue_num))
+for row in sheet.iter_rows(min_row=2, min_col=1, max_col=3, values_only=True):
+    issue_num = str(row[0])
+    sentence = str(row[2])
+    print("Extracted sentence:", sentence)
+    print("Extracted issue number:", issue_num)
+
+    # Normalize the sentence by stemming and lemmatizing
+    normalized_sentence = ' '.join([
+        stemmer.stem(lemmatizer.lemmatize(word.lower(), wordnet.VERB))
+        for word in sentence.split()
+    ])
+
+    # Check if the normalized sentence contains any of the phrases
+    if contains_phrase(normalized_sentence, PHRASES):
+        # Store the sentence in the "sentences" list
+        sentences.append((sentence,))
+        # Store the issue number in the "issueCodes" list
+        issueCodes.append((issue_num,))
 
 # Connect to the database
 with connect_to_database(DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD) as conn:
     # Create a cursor object
     with conn.cursor() as cursor:
         # Insert the sentences into the "sentences" table
+        print("Inserting sentences...")
         cursor.executemany(
-            "INSERT INTO sentences (issue_num, sentence) VALUES (%s, %s)", sentences)
+            "INSERT INTO sentences (sentence) VALUES (%s)", sentences)
 
         # Insert the issue codes into the "issueCodes" table
+        print("Inserting issue codes...")
         cursor.executemany(
-            "INSERT INTO issueCodes (issue_num, code) VALUES (%s, %s)", issueCodes)
+            "INSERT INTO issueCodes (code) VALUES (%s)", issueCodes)
 
     # Commit the changes to the database
     conn.commit()
+    print("Data insertion completed.")
