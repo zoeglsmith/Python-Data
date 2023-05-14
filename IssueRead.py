9246805c240e7f51c24a6b3a3348ca30b1eb2eb4
smizo424 <smizo424@student.otago.ac.nz>
2023-05-14 15:13:30 +1200

    stores issues and sentences but dupliates atm

--- IssueRead.py
+++ IssueRead.py
@@ -7,6 +7,7 @@ import os
 import re
 import psycopg2
 import openpyxl
+import difflib
 
 # Set the SSL certificate file path
 ssl._create_default_https_context = ssl._create_unverified_context
@@ -22,12 +23,12 @@ DATABASE_USER = "zoe"
 DATABASE_PASSWORD = "password"
 
 # Constants for specific phrases
-phrases = ["Bug", "Error", "Problem", "Feature", "Enhancement", "Issue", "Fix",
-           "Crash", "Support", "Request", "Improve", "Unable", "Broken", "Documentation", "Performance"]
+phrases = {"issue", "problem", "bug", "error", "fix", "solution", "cause", "concern",
+           "dependency", "implementation", "bug fix", "code", "functionality", "support", "validate", "detected", "crash", "request", "improve",
+           "unable", "broken", "documentation", "performance", "caused by", "validate", "logic", "code", "functionality"}
+
 
 # Function to check if a sentence contains any of the phrases
-
-
 def contains_phrase(sentence, phrases):
     for phrase in phrases:
         pattern = r"\b" + re.escape(phrase) + r"\b"
@@ -35,9 +36,23 @@ def contains_phrase(sentence, phrases):
             return True
     return False
 
+
+# Function to find matching phrases with variations (typos, plural forms, different endings)
+def find_matching_phrases(phrase, phrases, lemmatizer):
+    matches = []
+    phrase_lemma = lemmatizer.lemmatize(phrase.lower())
+    for p in phrases:
+        p_lemma = lemmatizer.lemmatize(p.lower())
+        if p_lemma == phrase_lemma:
+            matches.append(p)
+        elif p.lower() == phrase.lower() + "s" or p.lower() + "s" == phrase.lower():
+            matches.append(p)
+        elif difflib.SequenceMatcher(None, p.lower(), phrase.lower()).ratio() > 0.8:
+            matches.append(p)
+    return matches
+
+
 # Function to connect to the database using a context manager
-
-
 def connect_to_database(database_name, database_user, database_password):
     conn = psycopg2.connect(
         host="localhost",
@@ -58,9 +73,12 @@ workbook = openpyxl.load_workbook('dataset.xlsx')
 # Select the first sheet in the workbook
 sheet = workbook.active
 
-# Lists to store sentences and issue codes
+# Lists to store unique sentences and issue codes
 sentences = []
 issueCodes = []
+
+# Dictionary to store variations of phrase words found in the sentences
+phrase_variations = {}
 
 # Check each sentence for the phrases
 for row in sheet.iter_rows(min_row=2, min_col=1, max_col=3, values_only=True):
@@ -71,16 +89,26 @@ for row in sheet.iter_rows(min_row=2, min_col=1, max_col=3, values_only=True):
 
     # Normalize the sentence by stemming and lemmatizing
     normalized_sentence = ' '.join([
-        stemmer.stem(lemmatizer.lemmatize(word.lower(), wordnet.VERB))
+        stemmer.stem(lemmatizer.lemmatize(word.lower()))
         for word in sentence.split()
     ])
 
-    # Check if the normalized sentence contains any of the phrases
-    if contains_phrase(normalized_sentence, phrases):
-        # Store the sentence in the "sentences" list
-        sentences.append((sentence,))
-        # Store the issue number in the "issueCodes" list
-        issueCodes.append((issue_num,))
+    # Check if the normalized sentence contains any of the phrases or their variations
+    for phrase in phrases:
+        if contains_phrase(normalized_sentence, [phrase]):
+            # Store the sentence in the "sentences" list
+            sentences.append(sentence)
+            # Store the issue number in the "issueCodes" list
+            issueCodes.append(issue_num)
+
+            # Find variations of the phrase in the sentence
+            matching_phrases = find_matching_phrases(
+                phrase, phrases, lemmatizer)
+
+            if matching_phrases:
+                phrase_variations[phrase] = matching_phrases
+                print("Found variations for phrase:", phrase)
+                print("Variations:", matching_phrases)
 
 # Connect to the database
 with connect_to_database(DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD) as conn:
@@ -91,13 +119,20 @@ with connect_to_database(DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD) as con
             # Insert the sentences into the "sentences" table
             print("Inserting sentences...")
             cursor.executemany(
-                "INSERT INTO sentences (sentence) VALUES (%s)", sentences)
+                "INSERT INTO sentences (sentence) VALUES (%s)", [(s,) for s in sentences])
 
             # Insert the issue codes into the "issueCodes" table
             print("Inserting issue codes...")
             cursor.executemany(
-                "INSERT INTO issueCodes (code) VALUES (%s)", issueCodes)
+                "INSERT INTO issueCodes (code) VALUES (%s)", [(c,) for c in issueCodes])
 
     # Commit the changes to the database
     conn.commit()
     print("Data insertion completed.")
+
+
+# Print the variations of phrase words found in the sentences
+print("\nPhrase Variations:")
+for phrase, variations in phrase_variations.items():
+    print("Phrase:", phrase)
+    print("Variations:", variations)
