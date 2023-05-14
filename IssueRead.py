470daa2f786295584c2661e53ffb9d3015a33e00
smizo424 <smizo424@student.otago.ac.nz>
2023-05-14 15:22:44 +1200

    Code no longer stores duplicates

--- IssueRead.py
+++ IssueRead.py
@@ -8,6 +8,7 @@ import re
 import psycopg2
 import openpyxl
 import difflib
+from psycopg2 import pool
 
 # Set the SSL certificate file path
 ssl._create_default_https_context = ssl._create_unverified_context
@@ -21,11 +22,15 @@ nltk.download('wordnet')
 DATABASE_NAME = "issues"
 DATABASE_USER = "zoe"
 DATABASE_PASSWORD = "password"
+DATABASE_POOL_SIZE = 5
 
 # Constants for specific phrases
-phrases = {"issue", "problem", "bug", "error", "fix", "solution", "cause", "concern",
-           "dependency", "implementation", "bug fix", "code", "functionality", "support", "validate", "detected", "crash", "request", "improve",
-           "unable", "broken", "documentation", "performance", "caused by", "validate", "logic", "code", "functionality"}
+phrases = {
+    "issue", "problem", "bug", "error", "fix", "solution", "cause", "concern",
+    "dependency", "implementation", "bug fix", "code", "functionality", "support", "validate", "detected", "crash",
+    "request", "improve", "unable", "broken", "documentation", "performance", "caused by", "validate", "logic",
+    "code", "functionality"
+}
 
 
 # Function to check if a sentence contains any of the phrases
@@ -52,15 +57,16 @@ def find_matching_phrases(phrase, phrases, lemmatizer):
     return matches
 
 
-# Function to connect to the database using a context manager
-def connect_to_database(database_name, database_user, database_password):
-    conn = psycopg2.connect(
+# Function to connect to the database using a connection pool
+def create_connection_pool():
+    return psycopg2.pool.SimpleConnectionPool(
+        minconn=1,
+        maxconn=DATABASE_POOL_SIZE,
         host="localhost",
-        database=database_name,
-        user=database_user,
-        password=database_password
+        database=DATABASE_NAME,
+        user=DATABASE_USER,
+        password=DATABASE_PASSWORD
     )
-    return conn
 
 
 # Initialize stemmer and lemmatizer
@@ -73,12 +79,22 @@ workbook = openpyxl.load_workbook('dataset.xlsx')
 # Select the first sheet in the workbook
 sheet = workbook.active
 
-# Lists to store unique sentences and issue codes
-sentences = []
-issueCodes = []
+# Sets to store unique sentences and issue codes
+sentences = set()
+issueCodes = set()
 
 # Dictionary to store variations of phrase words found in the sentences
 phrase_variations = {}
+
+# Preprocess phrases and create variations
+preprocessed_phrases = [stemmer.stem(
+    lemmatizer.lemmatize(p.lower())) for p in phrases]
+for phrase in phrases:
+    matching_phrases = find_matching_phrases(phrase, phrases, lemmatizer)
+    if matching_phrases:
+        phrase_variations[phrase] = matching_phrases
+        print("Found variations for phrase:", phrase)
+        print("Variations:", matching_phrases)
 
 # Check each sentence for the phrases
 for row in sheet.iter_rows(min_row=2, min_col=1, max_col=3, values_only=True):
@@ -88,47 +104,50 @@ for row in sheet.iter_rows(min_row=2, min_col=1, max_col=3, values_only=True):
     print("Extracted issue number:", issue_num)
 
     # Normalize the sentence by stemming and lemmatizing
-    normalized_sentence = ' '.join([
-        stemmer.stem(lemmatizer.lemmatize(word.lower()))
-        for word in sentence.split()
-    ])
+    normalized_sentence = ' '.join(
+        [stemmer.stem(lemmatizer.lemmatize(word.lower())) for word in sentence.split()])
 
-    # Check if the normalized sentence contains any of the phrases or their variations
-    for phrase in phrases:
-        if contains_phrase(normalized_sentence, [phrase]):
-            # Store the sentence in the "sentences" list
-            sentences.append(sentence)
-            # Store the issue number in the "issueCodes" list
-            issueCodes.append(issue_num)
+    # Check if the normalized sentence contains any of the phrases
+    if contains_phrase(normalized_sentence, preprocessed_phrases):
+        # Store the sentence in the "sentences" set
+        sentences.add(sentence)
+        # Store the issue number in the "issueCodes" set
+        issueCodes.add(issue_num)
 
-            # Find variations of the phrase in the sentence
-            matching_phrases = find_matching_phrases(
-                phrase, phrases, lemmatizer)
 
-            if matching_phrases:
-                phrase_variations[phrase] = matching_phrases
-                print("Found variations for phrase:", phrase)
-                print("Variations:", matching_phrases)
+# Connect to the database using the connection pool
+conn_pool = create_connection_pool()
 
-# Connect to the database
-with connect_to_database(DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD) as conn:
+# Get a connection from the pool
+conn = conn_pool.getconn()
+
+try:
     # Create a cursor object
-    with conn.cursor() as cursor:
-        # Check if there are any valid sentences and issue codes to insert
-        if sentences and issueCodes:
-            # Insert the sentences into the "sentences" table
-            print("Inserting sentences...")
-            cursor.executemany(
-                "INSERT INTO sentences (sentence) VALUES (%s)", [(s,) for s in sentences])
+    cursor = conn.cursor()
 
-            # Insert the issue codes into the "issueCodes" table
-            print("Inserting issue codes...")
-            cursor.executemany(
-                "INSERT INTO issueCodes (code) VALUES (%s)", [(c,) for c in issueCodes])
+    # Check if there are any valid sentences and issue codes to insert
+    if sentences and issueCodes:
+        # Convert sets to lists for bulk insertion
+        sentence_list = list(sentences)
+        issue_code_list = list(issueCodes)
+
+        # Insert the sentences into the "sentences" table
+        print("Inserting sentences...")
+        cursor.executemany("INSERT INTO sentences (sentence) VALUES (%s)", [
+                           (s,) for s in sentence_list])
+
+        # Insert the issue codes into the "issueCodes" table
+        print("Inserting issue codes...")
+        cursor.executemany("INSERT INTO issueCodes (code) VALUES (%s)", [
+                           (c,) for c in issue_code_list])
 
     # Commit the changes to the database
     conn.commit()
     print("Data insertion completed.")
+
+finally:
+    # Release the connection back to the pool
+    conn_pool.putconn(conn)
 
 
 # Print the variations of phrase words found in the sentences
